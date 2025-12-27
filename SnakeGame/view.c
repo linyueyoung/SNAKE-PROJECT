dfce1fb8a5c808bbe62bbef9c9212b256a362ef9
wqd1206 <t114360245@ntut.org.tw>
2025-12-27 13:09:29 +0800

    1227

---
+++ SnakeGame/view.c
@@ -0,0 +1,21 @@
+#include "view.h"
+
+void gotoXY(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+void hideCursor() {
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 1;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+void initScreen() {
+    system("mode con cols=100 lines=30");
+    system("title Snake Project");
+    hideCursor();
+}
